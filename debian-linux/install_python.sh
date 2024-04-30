0e601631a5b767ea58710a5c15f7598323d860ed
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2024-04-29 21:23:08 -0400

    Updated Python install script

--- debian-linux/install_python.sh
+++ debian-linux/install_python.sh
@@ -1,12 +1,15 @@
 set -e
 
+# Install build dependencies
+sudo apt update && sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 # Install pyenv
 curl https://pyenv.run | bash
-
-# Install build dependencies
-sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev curl \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+cat bashrc_python.txt >> ~/.bashrc
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 # Install the LTS Python
 pyenv install 3.12
