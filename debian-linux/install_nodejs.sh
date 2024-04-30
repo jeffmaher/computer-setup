1b4a141204e63ac630b373f82819128ad34ca1ad
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2024-04-29 21:31:06 -0400

    Added Node.js installation

---
+++ debian-linux/install_nodejs.sh
@@ -0,0 +1,14 @@
+set -e
+
+# Download and install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+\. "$NVM_DIR/nvm.sh"
+\. "$NVM_DIR/bash_completion"
+cat bashrc_nodejs.txt >> ~/.bashrc
+
+# Install the latest LTS
+nvm install --lts
+
+# Restart
+echo "Success! Please restart your terminal."
