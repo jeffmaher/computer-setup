e8d2d6b8c268d102ef96da2a539949f41cfa0cf5
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2024-05-11 06:07:36 -0400

    Added SSH setup

---
+++ debian-linux/setup_ssh.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+set -e
+
+# Install SSH
+sudo apt install ssh -y
+
+# Setup an SSH key
+ssh-keygen -t ed25519
+
+# Add SSH init to .bashrc
+cat bashrc_ssh.txt >> ~/.bashrc
+
+# Whenever you want to add your SSH key, run `ssh-add` (which will prompt you for your password)
