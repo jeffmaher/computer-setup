de4ae0efb8090461acb3e4647735e1eb411e133d
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2024-08-25 16:15:01 -0400

    Update NVM

--- debian-linux/install_nodejs.sh
+++ debian-linux/install_nodejs.sh
@@ -1,7 +1,7 @@
 set -e
 
 # Download and install NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 \. "$NVM_DIR/nvm.sh"
 \. "$NVM_DIR/bash_completion"
