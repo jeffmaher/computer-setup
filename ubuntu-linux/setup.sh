e3541abf2dbb1aa60cf8532c6167fdb0d4a6f42f
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-04-11 09:13:30 -0400

    Update Slack install and add -y flags to apt installs

--- ubuntu-linux/setup.sh
+++ ubuntu-linux/setup.sh
@@ -38,15 +38,15 @@ snap refresh
 
 status "Turn on Firewall"
 ufw enable
-apt install gufw
+apt install gufw  -y
 
 status "Attach to Ubuntu Pro"
 pro attach $UBUNTU_PRO_KEY
 pro disable livepatch
 
 status "Install VIM as text editor"
-apt install vim
-update-alternatives --set editor /usr/bin/vim
+apt install vim  -y
+update-alternatives --set editor /usr/bin/vim.basic
 
 status "Install DNS Over TLS and DNSSEC provider"
 cat ../configs/dns_config.conf >> /etc/systemd/resolved.conf
@@ -55,7 +55,7 @@ systemctl restart NetworkManager.service
 
 status "Install password manager"
 wget https://downloads.1password.com/linux/debian/amd64/stable/1password-latest.deb
-apt install ./1password-latest.deb
+apt install ./1password-latest.deb  -y
 post_setup "Setup password manager"
 
 status "Install Firefox DEB package (instead of snap)"
@@ -66,38 +66,33 @@ wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
 cat ../configs/firefox_repo.conf | tee /etc/apt/preferences.d/mozilla
 apt update
-apt install firefox
+apt install firefox  -y
 post_setup "Setup Firefox"
 
 
 status "Install Solaar for Logitech keyboards and mice"
 add-apt-repository ppa:solaar-unifying/stable
 apt update
-apt install solaar
+apt install solaar  -y
 post_setup "Disable Solaar from always running in the Startup App"
 post_setup "Configure Solaar to use F keys"
 
 
 status "Install Camera app"
-apt install gnome-snapshot
+apt install gnome-snapshot  -y
 
 
 status "Install GNOME Extensions app and browser connector"
-apt install gnome-browser-connector
+apt install gnome-browser-connector  -y
 
 
 status "Install Slack"
-# snap install slack
-status "Install Slack (since Snap package has login retention issue)"
-curl -s https://packagecloud.io/install/repositories/slacktechnologies/slack/script.deb.sh | bash
-apt update
-apt install 
 post_setup "Setup Slack accounts"
 
 
 status "Install Zoom video conferencing"
 wget https://zoom.us/client/latest/zoom_amd64.deb
-apt install ./zoom_amd64.deb
+apt install ./zoom_amd64.deb  -y
 post_setup "Login to Zoom"
 
 
@@ -106,7 +101,7 @@ wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signa
 cat signal-desktop-keyring.gpg |  tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | tee /etc/apt/sources.list.d/signal-xenial.list
 apt update
-apt install signal-desktop
+apt install signal-desktop  -y
 post_setup "Link Signal to phone"
 
 
@@ -126,12 +121,12 @@ status "Install Mullvad VPN"
 curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
 echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
 apt update
-apt install mullvad-vpn
+apt install mullvad-vpn  -y
 post_setup "Login to Mullvad VPN"
 
 
 status "Install Git"
-apt install git
+apt install git  -y
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
 
@@ -161,17 +156,17 @@ post_setup "Setup Visual Studio Code"
 
 
 status "Install Docker"
-apt install ca-certificates curl
+apt install ca-certificates curl  -y
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-apt install docker-desktop
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin  -y
+apt install docker-desktop  -y
 cat ../configs/docker_commands.sh >> ~/.bashrc
 systemctl disable docker.service docker.socket
-apt install pass
+apt install pass  -y
 post_setup "Generate a GPG key"
 post_setup "Setup pass storage (to enable Docker login)"
 post_setup "Login to Docker account"
@@ -192,7 +187,7 @@ systemctl enable disable-usb-wakeup.service
 
 status "Install Steam"
 wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb
-apt install ./steam.deb
+apt install ./steam.deb  -y
 post_setup "Login to Steam and configure Proton"
 
 
