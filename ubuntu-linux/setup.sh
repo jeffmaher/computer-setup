c5d754cfe558abed72f09cca49f93bc5e7262491
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-04-11 09:27:32 -0400

    Get Docker Desktop package

--- ubuntu-linux/setup.sh
+++ ubuntu-linux/setup.sh
@@ -163,7 +163,8 @@ chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin  -y
-apt install docker-desktop  -y
+wget "https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb"
+apt install ./docker-desktop-amd64.deb  -y
 cat ../configs/docker_commands.sh >> ~/.bashrc
 systemctl disable docker.service docker.socket
 apt install pass  -y
