295ee719bc23bd67f74ca9e8cffa2f7309bdc573
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-05-02 20:36:49 -0400

    Remove checklist/post_setup commands

--- fedora-linux/setup.sh
+++ fedora-linux/setup.sh
@@ -26,13 +26,6 @@ status() {
     declare -g current_step=$((current_step + 1))
 }
 
-checklist=""
-post_setup() {
-    declare -g checklist="$checklist\n- $1"
-}
-
-
-
 # --- SYSTEM AND FOUNDATIONAL ITEMS ---
 status "Update system packages"
 sudo dnf update --refresh -y
@@ -46,8 +39,6 @@ sudo hostnamectl set-hostname $SYSTEM_NAME
 
 status "Install Solaar for managing Logitech keyboards and mice"
 sh install-solaar.sh
-checklist "Solaar: Setup keyboard and mouse bindings"
-checklist "Solaar: Verify that it isn't starting up at startup"
 
 status "Install GNOME Extensions app"
 sh install-gnome-extensions-app.sh
@@ -58,12 +49,9 @@ sh install-gnome-tweaks.sh
 
 status "Configure Git"
 sh config-git.sh $GIT_USER_EMAIL $GIT_USER_EMAIL
-checklist "Git: Create SSH key and upload public key to services"
 
 status "Configure screenshots"
 sh config-screenshots.sh
-checklist "Screenshots: Enable showing the pointer"
-
 
 # --- APPS ---
 status "Remove default apps"
@@ -75,32 +63,23 @@ sh install-vim.sh
 
 status "Install Mozilla Firefox"
 sh install-firefox.sh
-checklist "Mozilla Firefox: Setup profiles"
-checklist "Mozilla Firefox: Configure password manager"
-checklist "Mozilla Firefox: Configure privacy settings"
 
 status "Install 1Password"
 sh install-1password.sh
 
 status "Install Visual Studio Code"
-flatpak install flathub com.visualstudio.code -y
-#flatpak install flathub com.vscodium.codium -y
-checklist "Visual Studio Code: Import or sync settings"
+#flatpak install flathub com.visualstudio.code -y
+flatpak install flathub com.vscodium.codium -y
+
 
 status "Install Google Chrome"
 sh install-google-chrome.sh
-checklist "Google Chrome: Setup profiles"
-checklist "Google Chrome: Configure password manager"
-checklist "Google Chrome: Configure privacy settings"
-checklist "Google Chrome: Setup PWAs"
 
-# Switched to browser PWA
 # status "Install Slack chat tool"
 # sh install-slack.sh
 
 status "Install Signal Private Messenger"
 sh install-signal.sh
-checklist "Signal: Sync with phone"
 
 status "Install Pinta for light image editings and screenshot markup"
 sh install-pinta.sh
@@ -114,20 +93,10 @@ sh install-lossless-cut.sh
 
 status "Install Mullvad for VPNing"
 sh install-mullvad.sh
-checklist "Mullvad: Login to VPN account"
-
-
 
 
 status "Install Docker for OS/runtime containers"
 sh install-docker-desktop.sh
-checklist "Docker Desktop: Confirm that Docker runtime isn't always running"
-checklist "Docker Desktop: Initialize pass. See https://docs.docker.com/desktop/setup/sign-in/#signing-in-with-docker-desktop-for-linux"
-checklist "Docker Desktop: Login"
-
 
 # TODO VLC if GNOME Videos (i.e. Totem) isn't working well
-# TODO Steam
 # TODO Disable USB Wakeup
-
-
--- ubuntu-linux/setup-apps.sh
+++ ubuntu-linux/setup-apps.sh
@@ -8,12 +8,7 @@ set -e
 current_step=0
 status() {
     echo "##### [$current_step]: $1 #####"
-    current_step=current_step+1
-}
-
-checklist=""
-post_setup() {
-    checklist="$checklist\n- $1"
+    declare -g current_step=$((current_step + 1))
 }
 
 # -- INSTALL APPS --
@@ -22,24 +17,15 @@ sh install-vim.sh
 
 status "Install Solaar for managing Logitech keyboards and mice"
 sh install-solaar.sh
-checklist "Solaar: Setup keyboard and mouse bindings"
-checklist "Solaar: Verify that it isn't starting up at startup"
 
 status "Install GNOME Extensions app"
 sh install-gnome-extensions-app.sh
 
 status "Install Mozilla Firefox"
 sh install-firefox.sh
-checklist "Mozilla Firefox: Setup profiles"
-checklist "Mozilla Firefox: Configure password manager"
-checklist "Mozilla Firefox: Configure privacy settings"
 
 status "Install Google Chrome"
 sh install-google-chrome.sh
-checklist "Google Chrome: Setup profiles"
-checklist "Google Chrome: Configure password manager"
-checklist "Google Chrome: Configure privacy settings"
-checklist "Google Chrome: Setup PWAs"
 
 status "Install 1Password"
 sh install-1password.sh
@@ -49,15 +35,12 @@ sh install-vscode.sh
 
 status "Install Solaar for Logitech keyboards and mice"
 sh install-solaar.sh
-post_setup "Disable Solaar from always running in the Startup App"
-post_setup "Configure Solaar to use F keys"
 
 status "Install Camera for testing webcam"
 sh install-cameria.sh
 
 status "Install Signal Private Messenger"
 sh install-signal.sh
-checklist "Signal: Sync with phone"
 
 status "Install Pinta for light image editings and screenshot markup"
 sh install-pinta.sh
@@ -73,41 +56,18 @@ sh install-lossless-cut.sh
 
 status "Install Mullvad for VPNing"
 sh install-mullvad.sh
-checklist "Mullvad: Login to VPN account"
 
 status "Install Zoom Video Conferencing"
 sh install-zoom.sh
-checklist "Zoom: Login"
 
 status "Install Google Cloud CLI"
 sh install-gcp-cli.sh
-post_setup "GCP: Login"
 
 status "Install GNOME Videos/Totem"
 sh install-video-player.sh
 
 status "Install Docker"
 sh install-docker-desktop.sh
-post_setup "Docker: Generate a GPG key"
-post_setup "Docker: Setup pass storage (to enable Docker login)"
-post_setup "Docker: Login"
 
 status "Install LibreOffice for opening MS Office docs and more complex PDF editing"
 sh install-libre-office.sh
-
-
-# status "Install Steam"
-# sh install-steam.sh
-# post_setup "Login to Steam and configure Proton"
-
-
-post_setup "Configure keyboard shortcuts (system and window tiling extension)"
-post_setup "Configure settings in the Settings app"
-post_setup "Configure Terminal app preferences"
-post_setup "Get login screen to show up on the correct monitor"
-post_setup "Install programming languages"
-post_setup "Evaluate X11 vs. Wayland depending on the system"
-
-cd ..
-echo "#### POST SETUP CHECKLIST ####"
-echo $checklist
