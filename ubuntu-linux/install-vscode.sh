a6f5f4da51f5a54e38d2e57ea9a820382a5e671a
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-05-03 06:27:11 -0400

    Changed VSCode to a Snap

--- ubuntu-linux/install-vscode.sh
+++ ubuntu-linux/install-vscode.sh
@@ -1,4 +1,7 @@
 set -e 
 
+# Flatpak is a bit overly containerized, use Snap
 # flatpak install flathub com.visualstudio.code -y
-flatpak install flathub com.vscodium.codium -y
+# flatpak install flathub com.vscodium.codium -y
+
+snap install code
