5e7dbc64d61ae9246f52d9bc01335df7fcf60022
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-05-04 09:49:44 -0400

    Changed VPN to Mozilla VPN

---
+++ ubuntu-linux/install-vpn.sh
@@ -0,0 +1,7 @@
+set -e
+
+sudo install -d -m 0755 /etc/apt/keyrings 
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null 
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+sudo apt update
+sudo apt install mozillavpn -y
