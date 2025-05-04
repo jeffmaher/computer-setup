6bf8171f75d67f2a957c12735120785a07a1fea5
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-05-04 09:46:52 -0400

    Rename the VPN installer

--- ubuntu-linux/install-mullvad.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-sudo apt install curl
-sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
-echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
-sudo apt update
-sudo apt install mullvad-vpn
