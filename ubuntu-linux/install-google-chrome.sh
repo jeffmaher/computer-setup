b12c6bcd103be587fd7aba412b05579d57af7e4e
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-05-13 10:44:00 -0400

    Ubuntu: Switch Chrome install to Debian package

--- ubuntu-linux/install-google-chrome.sh
+++ ubuntu-linux/install-google-chrome.sh
@@ -1,8 +1,13 @@
 set -e
 
-flatpak install flathub com.google.Chrome -y
+DOWNLOAD_LOCATION=/var/tmp/google-chrome-stable_current_amd64.deb
 
+# Official installation for Debian package
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O $DOWNLOAD_LOCATION
+sudo apt install $DOWNLOAD_LOCATION -y
 
-mkdir -p ~/.local/share/applications
-mkdir -p ~/.local/share/icons
-flatpak override --user --filesystem=~/.local/share/icons --filesystem=~/.local/share/applications com.google.Chrome
+# Flatpak FlatHub install
+# flatpak install flathub com.google.Chrome -y
+# mkdir -p ~/.local/share/applications
+# mkdir -p ~/.local/share/icons
+# flatpak override --user --filesystem=~/.local/share/icons --filesystem=~/.local/share/applications com.google.Chrome
