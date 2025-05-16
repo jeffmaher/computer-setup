6c4146478dd22e638631e7740934b7e833782d0b
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-05-15 20:24:43 -0400

    Move 1Password back to Debian package

--- ubuntu-linux/install-1password.sh
+++ ubuntu-linux/install-1password.sh
@@ -1,4 +1,24 @@
 set -e
 
+# --- Debian package installation ---
+# - Unlike snap or flatpak, this can communicate with other applications (like DEB installed browsers)
+
+# Add key for apt repo
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+
+# Add apt repo
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+
+# Add the debsig-verify policy
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+# Install 1Password
+sudo apt update && sudo apt install 1password -y
+
+
+# --- Flatpak Installation ---
 # sudo needed since it's install a new repository
-sudo flatpak install https://downloads.1password.com/linux/flatpak/1Password.flatpakref -y
+# sudo flatpak install https://downloads.1password.com/linux/flatpak/1Password.flatpakref -y
