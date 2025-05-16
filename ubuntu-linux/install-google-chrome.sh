bb5515093c22a2612b4ff79ecffb1f5c793238ac
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-05-16 08:29:08 -0400

    Cleanup code for Google Chrome install

--- ubuntu-linux/install-google-chrome.sh
+++ ubuntu-linux/install-google-chrome.sh
@@ -1,12 +1,12 @@
 set -e
 
-DOWNLOAD_LOCATION=/var/tmp/google-chrome-stable_current_amd64.deb
 
-# Official installation for Debian package
+# --- Debian Package Install ---
+DOWNLOAD_LOCATION=/var/tmp/google-chrome-stable_current_amd64.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O $DOWNLOAD_LOCATION
 sudo apt install $DOWNLOAD_LOCATION -y
 
-# Flatpak FlatHub install
+# --- Flatpak FlatHub install ---
 # flatpak install flathub com.google.Chrome -y
 # mkdir -p ~/.local/share/applications
 # mkdir -p ~/.local/share/icons
