8665200e2c911dde8bf6985cb5427bf3310c6efe
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-06-16 12:48:05 -0400

    Steam: Local network sharing

--- ubuntu-linux/install-steam.sh
+++ ubuntu-linux/install-steam.sh
@@ -3,3 +3,7 @@ set -e
 DOWNLOAD_LOCATION=/var/tmp/steam.deb
 wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb -O $DOWNLOAD_LOCATION
 sudo apt install $DOWNLOAD_LOCATION  -y
+
+# Open ports for local network download sharing
+sudo ufw allow 27040/tcp
+sudo ufw allow 27031:27036/udp
