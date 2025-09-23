01e2745f87dddf04d8569049b3168fc4f19aba14
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-09-23 13:45:03 -0400

    Move GCP install script

--- ubuntu-linux/install-gcp-cli.sh
+++ ubuntu-linux/install-gcp-cli.sh
@@ -1,8 +1,17 @@
 set -e
+# ** Script ased on https://cloud.google.com/sdk/docs/install#deb **
 
-
+# Install dependencies
 sudo apt-get install apt-transport-https ca-certificates gnupg curl -y
+
+# Install Google Cloud public key
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+
+# Add package source to apt
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo apt update
-sudo apt install google-cloud-cli -y
+
+# Install the CLI
+sudo apt-get update && sudo apt-get install google-cloud-cli -y
+
+# Login
+gcloud init
--- utilities/create-ssh-key.sh
+++
@@ -1,5 +0,0 @@
-# USAGE: sh create-ssh-key.sh <email>
-
-set -e
-
-ssh-keygen -t ed25519 -C $1
