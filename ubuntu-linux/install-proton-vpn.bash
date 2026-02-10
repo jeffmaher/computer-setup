7b2e095c91125c917b39be1d5daeb7470332d451
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2026-02-10 09:39:58 -0500

    Fix for Proton VPN installer on Ubuntu

--- ubuntu-linux/install-proton-vpn.bash
+++ ubuntu-linux/install-proton-vpn.bash
@@ -4,9 +4,9 @@ set -e
 
 DOWNLOAD_LOCATION=/var/tmp/protonvpn-stable-release_1.0.8_all.deb
 wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb -O $DOWNLOAD_LOCATION
-sudo dpkg -i ./protonvpn-stable-release_1.0.8_all.deb && sudo apt update
-sudo apt install proton-vpn-gnome-desktop
-sudo apt install gnome-shell-extension-appindicator
+sudo dpkg -i $DOWNLOAD_LOCATION && sudo apt update
+sudo apt install proton-vpn-gnome-desktop -y
+sudo apt install gnome-shell-extension-appindicator -y
 
 
 # Removal instructions
