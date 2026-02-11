a05127c2edc9d48a5ca076c2477be6fa92fde169
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2026-02-11 17:02:25 -0500

    Add/update instructions for screen capture tools for Ubuntu

---
+++ ubuntu-linux/install-kooha.sh
@@ -0,0 +1,5 @@
+# Installs Vokoscreen NG for screen recording
+
+set -e
+
+flatpak install flathub io.github.seadve.Kooha -y
---
+++ ubuntu-linux/install-vokoscreen.sh
@@ -0,0 +1,5 @@
+# Installs Vokoscreen NG for screen recording
+
+set -e
+
+flatpak install flathub com.github.vkohaupt.vokoscreenNG -y
